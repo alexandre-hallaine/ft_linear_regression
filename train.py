f15fcffe3899548d4cc830f401a63e0f22de7013
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-06-06 15:18:49 +0200

    add: training graph

--- train.py
+++ train.py
@@ -4,6 +4,13 @@ import estimate
 import plotly.graph_objects as go
 
 
+def infinity():
+    i = 0
+    while True:
+        yield i
+        i += 1
+
+
 def normalisation(data):
     return [(row - min(data)) / (max(data) - min(data)) for row in data]
 
@@ -32,23 +39,49 @@ with open('data.csv', 'r') as file:
 
 print('Training...')
 
-theta = [1, 1]
-learning_rate = 1
-print('Learning rate:', learning_rate)
+theta = [0, 0]
+learning_rate = 0.1
+normalised_mileages = normalisation(mileages)
+normalised_prices = normalisation(prices)
 
-while True:
-    new = train([normalisation(mileages), normalisation(prices)],
+trained_figure = go.Figure()
+trained_figure.update_layout(
+    xaxis_title='Mileage', yaxis_title='Price', title='Training')
+trained_figure.add_trace(go.Scatter(x=normalised_mileages,
+                                    y=normalised_prices, mode='markers', name='Data'))
+
+print('Learning rate:', learning_rate)
+for i in infinity():
+    new = train([normalised_mileages, normalised_prices],
                 theta, learning_rate)
     if new == theta:
         break
     theta = new
     print('Theta Normalised:', theta, end='\r')
+
+    if i % int(1 / learning_rate) != 0:
+        continue
+
+    x = [min(normalised_mileages), max(normalised_mileages)]
+    y = [estimate.price(x, theta) for x in x]
+    trained_figure.add_trace(go.Scatter(x=x, y=y, mode='lines',
+                             name='Estimate {}'.format(i)))
 print()
 
 delta = [max(x) - min(x) for x in [mileages, prices]]
 theta[1] = theta[1] * delta[1] / delta[0]
 theta[0] = theta[0] * delta[1] + min(prices) - theta[1] * min(mileages)
 print('Theta Denormalised:', theta)
+
+x = [min(mileages), max(mileages)]
+y = [estimate.price(x, theta) for x in x]
+
+denormalised_figure = go.Figure()
+denormalised_figure.update_layout(xaxis_title='Mileage', yaxis_title='Price')
+denormalised_figure.add_trace(go.Scatter(x=mileages,
+                                         y=prices, mode='markers', name='Data'))
+denormalised_figure.add_trace(go.Scatter(
+    x=x, y=y, mode='lines', name='Estimate'))
 
 print('Writing result...')
 
@@ -60,12 +93,5 @@ with open('result.csv', 'w') as file:
 
 print('Done!')
 
-x = [min(mileages), max(mileages)]
-y = [estimate.price(x, theta) for x in x]
-
-fig = go.Figure()
-fig.update_layout(xaxis_title='Mileage', yaxis_title='Price')
-fig.add_trace(go.Scatter(x=mileages,
-              y=prices, mode='markers', name='Data'))
-fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='Estimate'))
-fig.show()
+trained_figure.show()
+denormalised_figure.show()
